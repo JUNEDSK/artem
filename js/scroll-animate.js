03fb9db46b6ba66d31d5df30d4e55dbbed3f1e4b
Manjesh R V <manjeshrv592@gmail.com>
2023-10-05 17:52:00 +0530

    Finished vertical page

---
+++ js/scroll-animate.js
@@ -0,0 +1,10 @@
+const allImg = document.querySelectorAll(".section-scroll-gallery img");
+const allCards = document.querySelectorAll(".desktop-content .scroll-card");
+
+ScrollTrigger.create({
+  trigger: ".section-scroll-content",
+  start: "top 30%",
+  end: "bottom bottom",
+  pin: ".section-scroll-gallery",
+  pinSpacing: false,
+});
