d8fd2de739c2f303cd9fa51f98ebd6a73559f11e
Manjesh R V <manjeshrv592@gmail.com>
2023-10-09 11:41:03 +0530

    test push

--- js/artem.js
+++ js/artem.js
@@ -1,18 +1,20 @@
 const allImg = document.querySelectorAll(".section-scroll-gallery img");
 const allCards = document.querySelectorAll(".desktop-content .scroll-card");
 
-ScrollTrigger.create({
-  trigger: ".section-scroll-content",
-  start: "top 30%",
-  end: "bottom bottom",
-  pin: ".section-scroll-gallery",
-  pinSpacing: false,
-});
+if (allImg.length > 0) {
+  ScrollTrigger.create({
+    trigger: ".section-scroll-content",
+    start: "top 30%",
+    end: "bottom bottom",
+    pin: ".section-scroll-gallery",
+    pinSpacing: false,
+  });
 
-ScrollTrigger.create({
-  trigger: ".overview",
-  start: "top top",
-  end: "bottom bottom",
-  pin: ".overview-side",
-  pinSpacing: false,
-});
+  ScrollTrigger.create({
+    trigger: ".overview",
+    start: "top top",
+    end: "bottom bottom",
+    pin: ".overview-side",
+    pinSpacing: false,
+  });
+}
