e9d8004abf50605d85c75d0b025f5802b035cf3b
Manjesh R V <manjeshrv592@gmail.com>
2023-10-09 16:20:09 +0530

    Added about, career and contact us page

--- js/artem.js
+++ js/artem.js
@@ -9,12 +9,25 @@ if (allImg.length > 0) {
     pin: ".section-scroll-gallery",
     pinSpacing: false,
   });
+}
 
-  ScrollTrigger.create({
-    trigger: ".overview",
-    start: "top top",
-    end: "bottom bottom",
-    pin: ".overview-side",
-    pinSpacing: false,
+ScrollTrigger.create({
+  trigger: ".overview",
+  start: "top top",
+  end: "bottom bottom",
+  pin: ".aside",
+  pinSpacing: false,
+});
+
+///////////////////////////////////////////////////////
+// Overview list
+const allOverviewList = document.querySelectorAll(".overview-list");
+
+if (allOverviewList.length !== 0) {
+  allOverviewList.forEach(list => {
+    const items = list.querySelectorAll(".overview-list-item");
+    if (items.length % 2 !== 0) return;
+
+    items[items.length - 2].style.borderBottom = "none";
   });
 }
