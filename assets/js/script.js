7cc3d12bcff09137b0c82a5ddcb161d06eacd415
Manjesh R V <manjeshrv592@gmail.com>
2023-10-31 10:46:43 +0530

    Added artem verticals page

--- assets/js/script.js
+++ assets/js/script.js
@@ -199,3 +199,26 @@ const swiper = new Swiper(".swiper", {
     },
   },
 });
+
+///////////////////////////////////////////////////
+// Vertcal section animation
+const allImg = document.querySelectorAll(".section-scroll-gallery img");
+const allCards = document.querySelectorAll(".desktop-content .scroll-card");
+
+if (allImg.length > 0) {
+  ScrollTrigger.create({
+    trigger: ".section-scroll-content",
+    start: "top 30%",
+    end: "bottom bottom",
+    pin: ".section-scroll-gallery",
+    pinSpacing: false,
+  });
+}
+
+ScrollTrigger.create({
+  trigger: ".overview",
+  start: "top top",
+  end: "bottom bottom",
+  pin: ".aside",
+  pinSpacing: false,
+});
